ac5f0790416905b2829a8cf84f343a3002b59d85
Primzark <starlod7696@gmail.com>
2025-03-04 14:31:51 +0100

    add

--- src/View/view-inscription.php
+++ src/View/view-inscription.php
@@ -1,7 +1,4 @@
-
-
-
-<?php 
+<?php
 include_once("../../templates/head.php");
 ?>
 
@@ -13,92 +10,120 @@ include_once("../../templates/head.php");
         <div class="row justify-content-center">
             <div class="col-6">
                 <div class="card">
-                    <div class="p-4">
+                    <div>
                         <h1 class="card-title text-center">Inscription</h1>
                         <form action="" method="post" novalidate>
                             <div>
                                 <label for="nom" class="form-label">Nom:</label>
                                 <span class="text-danger"><?= $errors['nom'] ?? '' ?></span>
-                                <input type="text" class="form-control" id="nom" name="nom" required>
-                                <?php if (isset($errors['nom'])): ?>
-                                    <div class="invalid-feedback">
-                                        <?= $errors['nom'] ?>
-                                    </div>
-                                <?php endif; ?>
+                                <input type="text"
+                                    class="form-control <?php echo isset($errors['nom']) ? 'is-invalid' : ''; ?>"
+                                    id="nom" name="nom" required>
                             </div>
+                            <?php if (isset($errors['nom'])): ?>
+                                <div class="invalid-feedback">
+                                    <?= $errors['nom'] ?>
+                                </div>
+                            <?php else: ?>
+                                <div class="valid-feedback">
+                                    Nom valide
+                                </div>
+                            <?php endif; ?>
 
                             <div class="mb-3">
                                 <label for="prenom" class="form-label">Prénom:</label>
                                 <span class="text-danger"><?= $errors['prenom'] ?? '' ?></span>
-                                <input type="text" class="form-control" id="prenom" name="prenom" required>
+                                <input type="text"
+                                    class="form-control <?php echo isset($errors['nom']) ? 'is-invalid' : ''; ?>"
+                                    id="nom" name="nom" required>
                                 <?php if (isset($errors['nom'])): ?>
                                     <div class="invalid-feedback">
                                         <?= $errors['nom'] ?>
                                     </div>
-                                <?php endif; ?>
-                            </div>
-
-                            <div class="mb-3">
-                                <label for="email" class="form-label">Email:</label>
-                                <span class="text-danger"><?= $errors['email'] ?? '' ?></span>
-                                <input type="email" class="form-control" id="email" name="email" required>
-                                <?php if (isset($errors['nom'])): ?>
-                                    <div class="invalid-feedback">
-                                        <?= $errors['nom'] ?>
+                                <?php else: ?>
+                                    <div class="valid-feedback">
+                                        Nom valide
                                     </div>
                                 <?php endif; ?>
-                            </div>
 
-                            <div class="mb-3">
-                                <label for="password" class="form-label">Mot de passe: (Maj, numb, min, special)</label>
-                                <span class="text-danger"><?= $errors['password'] ?? '' ?></span>
-                                <input type="password" class="form-control" id="password" name="password" required>
-                                <?php if (isset($errors['nom'])): ?>
-                                    <div class="invalid-feedback">
-                                        <?= $errors['nom'] ?>
-                                    </div>
-                                <?php endif; ?>
-                            </div>
-
-                            <div class="mb-3">
-                                <label for="confirm_password" class="form-label">Confirmez le mot de passe:</label>
-                                <span class="text-danger"><?= $errors['lastname'] ?? '' ?></span>
-                                <input type="password" class="form-control" id="confirm_password"
-                                    name="confirm_password" required>
+                                <div class="mb-3">
+                                    <label for="email" class="form-label">Email:</label>
+                                    <span class="text-danger"><?= $errors['email'] ?? '' ?></span>
+                                    <input type="email"
+                                        class="form-control <?php echo isset($errors['email']) ? 'is-invalid' : ''; ?>"
+                                        id="nom" name="email" required>
                                     <?php if (isset($errors['nom'])): ?>
-                                    <div class="invalid-feedback">
-                                        <?= $errors['nom'] ?>
-                                    </div>
-                                <?php endif; ?>
-                            </div>
+                                        <div class="invalid-feedback">
+                                            <?= $errors['nom'] ?>
+                                        </div>
+                                    <?php else: ?>
+                                        <div class="valid-feedback">
+                                            Nom valide
+                                        </div>
+                                    <?php endif; ?>
 
-                            <div class="mb-3">
-                                <label for="dob" class="form-label">Date de naissance:</label>
-                                <span class="text-danger"><?= $errors['lastname'] ?? '' ?></span>
-                                <input type="date" class="form-control" id="dob" name="dob" required>
-                            </div>
+                                    <div class="mb-3">
+                                        <label for="password" class="form-label">Mot de passe: (Maj, numb, min,
+                                            special)</label>
+                                        <span class="text-danger"><?= $errors['password'] ?? '' ?></span>
+                                        <input type="password"class="form-control <?php echo isset($errors['password']) ? 'is-invalid' : ''; ?>"
+                                        id="nom" name="password" required>
+                                        <?php if (isset($errors['nom'])): ?>
+                                            <div class="invalid-feedback">
+                                                <?= $errors['nom'] ?>
+                                            </div>
+                                        <?php else: ?>
+                                            <div class="valid-feedback">
+                                                Nom valide
+                                            </div>
+                                        <?php endif; ?>
 
-                            <div class="mb-3">
-                                <label for="genre" class="form-label">Genre:</label>
-                                <span class="text-danger"><?= $errors['genre'] ?? '' ?></span>
-                                <select class="form-select" id="genre" name="genre" required>
-                                    <option value="" selected disabled></option>
-                                    <option value="homme">Homme</option>
-                                    <option value="femme">Femme</option>
-                                    <option value="autre">Autre</option>
+                                        <div class="mb-3">
+                                            <label for="confirm_password" class="form-label">Confirmez le mot de
+                                                passe:</label>
+                                            <span class="text-danger"><?= $errors['lastname'] ?? '' ?></span>
+                                            <input type="password" class="form-control <?php echo isset($errors['confirm_password']) ? 'is-invalid' : ''; ?>"
+                                            id="nom" name="confirm_password" required>
+                                            <?php if (isset($errors['nom'])): ?>
+                                                <div class="invalid-feedback">
+                                                    <?= $errors['nom'] ?>
+                                                </div>
+                                            <?php else: ?>
+                                                <div class="valid-feedback">
+                                                    Nom valide
+                                                </div>
+                                            <?php endif; ?>
 
-                                </select>
-                            </div>
+                                            <div class="mb-3">
+                                                <label for="dob" class="form-label">Date de naissance:</label>
+                                                <span class="text-danger"><?= $errors['lastname'] ?? '' ?></span>
+                                                <input type="date" class="form-control <?php echo isset($errors['dob']) ? 'is-invalid' : ''; ?>"
+                                                id="nom" name="dob" required>
+                                            </div>
 
-                            <div class="mb-3">
-                                <input type="checkbox" class="form-check-input" id="terms" name="terms" required>
-                                <span class="text-danger"><?= $errors['terms'] ?? '' ?></span>
-                                <label class="form-check-label" for="terms">J'accepte les conditions
-                                    d'utilisation</label>
-                            </div>
+                                            <div class="mb-3">
+                                                <label for="genre" class="form-label">Genre:</label>
+                                                <span class="text-danger"><?= $errors['genre'] ?? '' ?></span>
+                                                <select class="form-control <?php echo isset($errors['genre']) ? 'is-invalid' : ''; ?>"
+                                                id="nom" name="genre" required>
+                                                    <option value="" selected disabled></option>
+                                                    <option value="homme">Homme</option>
+                                                    <option value="femme">Femme</option>
+                                                    <option value="autre">Autre</option>
 
-                            <a href="confirmation.php"><button type="submit"
-                                    class="btn btn-primary w-100">S'inscrire</button></a>
+                                                </select>
+                                            </div>
+
+                                            <div class="mb-3">
+                                                <input type="checkbox" class="form-check-input" id="terms" name="terms"
+                                                    required>
+                                                <span class="text-danger"><?= $errors['terms'] ?? '' ?></span>
+                                                <label class="form-check-label" for="terms">J'accepte les conditions
+                                                    d'utilisation</label>
+                                            </div>
+
+                                            <a href="confirmation.php"><button type="submit"
+                                                    class="btn btn-primary w-100">S'inscrire</button></a>
                         </form>
                     </div>
                 </div>
